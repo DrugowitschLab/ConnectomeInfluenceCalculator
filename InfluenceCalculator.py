4b0dd991bffe6cc073a2686dcebf4d7c523605dc
Jan Drugowitsch <jdrugo@gmail.com>
2025-04-23 09:12:17 -0400

    Ensured that example code is not run when importing module

--- InfluenceCalculator.py
+++ InfluenceCalculator.py
@@ -221,32 +221,37 @@ class InfluenceCalculator:
 
 		return influence_df
 
-"""
-Example use:
-1. Create InfluenceCalculator object using file path where dataset is saved
-4. Define seed categories
-5. Define silenced neuron ids
-6. Define seed neuron ids (per category)
-7. Build influence dataframe with influence scores (per category)
-8. Save dataframe (per category)
-9. Loop over all seed catgories
-"""
-ic = InfluenceCalculator('/banc_505_data.sqlite')
 
-# Define seed
-meta_column = 'seed_01'
-seed_categories = pd.unique(ic.meta[meta_column])
-seed_categories = [element for element in seed_categories if element != '']
+if __name__ == "__main__":
+    """
+    Example use:
+    1. Create InfluenceCalculator object using file path where dataset is saved
+    4. Define seed categories
+    5. Define silenced neuron ids
+    6. Define seed neuron ids (per category)
+    7. Build influence dataframe with influence scores (per category)
+    8. Save dataframe (per category)
+    9. Loop over all seed catgories
+    """
+    ic = InfluenceCalculator('banc_505_data.sqlite')
 
-# Get neuron ids to inhibit (sensory neurons in this case)
-silenced_neurons = ic.meta[ic.meta['super_class'].isin(['sensory', 'ascending_sensory'])].neuron_id
+    # Define seed
+    meta_column = 'seed_01'
+    seed_categories = pd.unique(ic.meta[meta_column])
+    seed_categories = [element for element in seed_categories if element != '']
 
-for seed_category in seed_categories:
-	# Get seed neuron ids
-	seed_ids = ic.meta[ic.meta[meta_column] == seed_category].neuron_id 
+    # Get neuron ids to inhibit (sensory neurons in this case)
+    silenced_neurons = ic.meta[
+        ic.meta['super_class'].isin(['sensory',
+                                     'ascending_sensory'])].neuron_id
 
-	influence_df = None
-	influence_df = ic.calculate_influence(seed_ids, silenced_neurons)
+    for seed_category in seed_categories:
+        # Get seed neuron ids
+        seed_ids = ic.meta[ic.meta[meta_column] == seed_category].neuron_id 
 
-	# Save the DataFrame to a CSV file
-	influence_df.to_csv(f"Influence/{seed_category}_influence.csv", index=False)
+        influence_df = None
+        influence_df = ic.calculate_influence(seed_ids, silenced_neurons)
+
+        # Save the DataFrame to a CSV file
+        influence_df.to_csv(f"Influence/{seed_category}_influence.csv",
+                            index=False)
