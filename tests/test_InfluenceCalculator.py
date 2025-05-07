940a1bdf8cdb401dad998b574808998134cf6139
ZakiAjabi <zaki_ajabi@hms.harvard.edu>
2025-05-07 18:24:26 -0400

    Update test_InfluenceCalculator.py

--- tests/test_InfluenceCalculator.py
+++ tests/test_InfluenceCalculator.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 def test_run_multiple_seeds():
 
     print("\nInitializing InfluenceCalculator object")
-    ic = InfluenceCalculator('toy_network_example.sqlite')
+    ic = InfluenceCalculator('test/toy_network_example.sqlite')
     print("InfluenceCalculator object created")
 
     # Define seed
